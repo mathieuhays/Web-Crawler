e797d3de286edc5e016958d9f0f2cda16e1057a0
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2024-06-16 10:53:53 +0200

    setup base structure and add normalizeURL

---
+++ crawl.js
@@ -0,0 +1,12 @@
+function normalizeURL(url) {
+    const urlObj = new URL(url)
+    let path = urlObj.pathname
+
+    if (path.length > 0 && path[path.length-1] === '/') {
+        path = path.slice(0, -1)
+    }
+
+    return `${urlObj.hostname}${path}`
+}
+
+export {normalizeURL};
---
+++ crawl.test.js
@@ -0,0 +1,24 @@
+import { expect, describe, it} from "@jest/globals"
+import {normalizeURL} from "./crawl.js";
+
+describe('normalizeURL', () => {
+    it('strips out protocol and trailing slash', () => {
+        expect(normalizeURL('http://site.example.com/blog/')).toBe('site.example.com/blog')
+    })
+
+    it('strip out secure protocol', () => {
+        expect(normalizeURL('https://www.example.com/blog')).toBe('www.example.com/blog')
+    })
+
+    it('strips out query string', () => {
+        expect(normalizeURL('http://example.com/?test')).toBe('example.com')
+        expect(normalizeURL('http://example.com/test/?test')).toBe('example.com/test')
+    })
+
+    it('throws when invalid url is provided', () => {
+        expect(() => {
+            normalizeURL('example.com/test')
+        }).toThrow(new TypeError('Invalid URL'))
+    })
+})
+
